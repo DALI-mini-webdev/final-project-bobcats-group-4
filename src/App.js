4063d1a2b82b1722108b584c3b1fa45cb4724252
learosen2 <lrosen@bates.edu>
2021-02-28 17:53:42 -0500

    more css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import calendar from "./components/calendar/calendar";
 import journal from "./components/journal/journal";
 import Navbar from "./components/Navbar/Navbar";
 import './App.css';
+import logoPic from "./components/images/Add a heading.png";
 
 function App() {
 
   return (
     <div className="App">
      <Navbar />
+     <img src = {logoPic} width={330} heigth ={410} /> 
     </div>
   );
 }
