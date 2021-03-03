a8ad1704c272c8b24401d692d4de563095d436cc
Katie Fletcher <77756012+kfletch2024@users.noreply.github.com>
2021-03-03 18:37:22 -0500

    revised navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
     render() {
         return(
             <nav className="NavbarItems" >
-                <h1 className="navbar-logo">website name here<i className="fas fa-edit"></i></h1>
+                <h1 className="navbar-logo">The Busy Bobcat<i className="fas fa-edit"></i></h1>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
 
